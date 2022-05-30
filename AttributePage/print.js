44d58a09a94a23758e4216b53dc05dd2654d562f
codemosh <omondihills02@gmail.com>
2022-05-30 12:05:45 +0300

    add

---
+++ AttributePage/print.js
@@ -0,0 +1,9 @@
+document.write("Hello World");
+console.log("Hello World");
+alert("here I come");
+var name="Ochola Hillary \nOmondi";
+var age=22;
+var sex="male";
+var school="JKUAT";
+var lateForSchool=true;
+console.log(name + age);
