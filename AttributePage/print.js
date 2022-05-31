fed96a00427ca7fd1aa898461049db3f9f87c6d2
codemosh <omondihills02@gmail.com>
2022-05-31 22:31:55 +0300

    add

--- AttributePage/print.js
+++ AttributePage/print.js
@@ -1,9 +1,41 @@
-document.write("Hello World");
-console.log("Hello World");
-alert("here I come");
-var name="Ochola Hillary \nOmondi";
-var age=22;
-var sex="male";
-var school="JKUAT";
-var lateForSchool=true;
-console.log(name + age);
+// document.write("Hello World");
+// console.log("Hello World");
+// alert("here I come");
+
+// var name="Ochola Hillary \nOmondi";
+// var age=22;
+// var sex="male";
+// var school="JKUAT";
+// var lateForSchool=true;
+// console.log(name + age);
+
+// var add=eval('10+5');
+// document.write(add);
+
+// var add='10'*'5';
+// document.write(add);
+
+// var add=10+5.2;
+// document.write(add);
+
+// var add=10/0;
+// document.write(add);
+
+// var x=10;
+// var y=5
+// x+=y;
+// document.write(x);
+
+/*comparison operator*/
+// var add=10;
+// document.write(add==10);
+
+//Conditional operator
+// var age=15;
+// var isAdult=(age<18) ? 'Too young': 'Old enough';
+// document.write(isAdult);
+// alert(isAdult);
+
+
+
+
