2475c3e1e78dcce1ce6f8586ac79cd49971c52d3
codemosh <omondihills02@gmail.com>
2022-06-03 12:52:07 +0300

    add

--- AttributePage/print.js
+++ AttributePage/print.js
@@ -31,11 +31,140 @@
 // document.write(add==10);
 
 //Conditional operator
-// var age=15;
-// var isAdult=(age<18) ? 'Too young': 'Old enough';
-// document.write(isAdult);
-// alert(isAdult);
+/*var age=15;
+var isAdult=(age<18) ? 'Too young': 'Old enough';
+document.write(isAdult);
+alert(isAdult);*/
+
+// CONDITIONALS
+
+// if, else if, else
+/*var status=1;
+var msg;
+if(status==1){
+   msg="Online";
+}
+else if(status==2){
+    msg="Away";
+}
+else{
+    msg="Offline";
+}
+console.log(msg);*/
+
+// Switch Case
+// var month=parseInt(readLine(), 10);
+/*var month=11;
+switch(month){
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+         console.log("March")
+         break;
+    case 4:
+         console.log("April")
+         break;
+    case 5:
+         console.log("May")
+         break;
+    case 6:
+          console.log("June")
+          break;
+    case 7:
+         console.log("July")
+         break;
+    case 8:
+         console.log("August")
+         break;
+    case 9:
+        console.log("September")
+        break;
+    case 10:
+         console.log("October")
+         break;
+    case 11:
+        console.log("November")
+        break;
+    case 12:
+       console.log("December")
+       break;
+    
+}*/
+
+// LOOPS
+
+// for(i=0;i<=10;i++){
+    // document.write(i +"<br>");
+    // console.log(i);
+// }
+
+// var expression=readLine();
+// for(i=1;i<=3;i++){
+    // console.log(expression)
+// }
+
+// Even numbers
+// for(i=0;i<=20;i+=2){
+    // console.log(i);
+    // document.write(i+'<br>')
+// }
+
+// var sec=5;
+// while(sec>=0){
+    // document.write(sec);
+    // sec--;
+// }
+
+// for(i=0;i<=10;i++){
+    // if(i==5){
+        // break;
+    // }
+    // document.write(i);
+// 
+// }
+
+// Do...while loop
+// i=0;
+// do{
+    // document.write(i);
+    // i++;
+// }
+// while(i<5);
 
 
+// FUNCTIONS
 
 
+// function hello(name, name2){
+    // alert("Hello there "+ name+" " + name2);
+// }
+// hello("Hillary", "Omondi");
+
+// function multiply(a, b){
+//    return a*b;
+// }
+// document.write(multiply(3,2));
+
+// ALERT, PROMPT, CONFIRM
+
+// var user=prompt("Hi there");
+// alert(user);
+
+// var like=confirm("Do you want to really leave the page");
+// if(like==true){
+    // document.write("Welcome back next time.");
+// } else{
+    // document.write("Thank you for staying with us.");
+// }
+
+function test(number){
+    while(number<5){
+        number++
+    }
+    return number;
+}
+alert(test(9));
