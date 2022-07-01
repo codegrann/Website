cf6517e57510309110e30343d1c197195ff0644d
codemosh <omondihills02@gmail.com>
2022-07-01 10:02:48 +0300

    rm

--- Javascript modules/print.js
+++
@@ -1,9 +0,0 @@
-// document.write("Hello World");
-// console.log("Hello World");
-// alert("here I come");
-var name="Ochola Hillary \nOmondi";
-var age=22;
-var sex="male";
-var school="JKUAT";
-var lateForSchool=true;
-console.log(lateForSchool);
